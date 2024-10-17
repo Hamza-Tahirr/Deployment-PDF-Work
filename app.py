0849b3fdaf3f39da890ea79494a130f2cfe3b36b
Hamza-Tahirr <hamzatahiramin2@gmail.com>
2024-10-17 14:49:55 +0500

    First commit

--- app.py
+++ app.py
@@ -241,4 +241,5 @@ def final(filename):
    return send_file(os.path.join(UPLOAD_FOLDER, filename), as_attachment=True)
 
 if __name__ == '__main__':
-   app.run(debug=True, port=5001)
+        port = int(os.environ.get('PORT', 5000))
+        app.run(debug=True, host='0.0.0.0', port=port)
